2a0d6ea5ce04f1c836313efb10568a1e549484da
Robert Castley <robert.castley@gmail.com>
2022-07-25 11:07:20 +0100

    Fixed telemtry typos

--- telemetry/server.py
+++ telemetry/server.py
@@ -1,41 +1,45 @@
 import socket
 import yaml
-from f1_2020_telemetry.packets import unpack_udp_packet
-
 import telemetry.splunk as metrics
+from f1_2020_telemetry.packets import unpack_udp_packet
 
 with open("config.yaml", "r") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
+
 def start_driver(driver_name):
-    print(f"driver {driver_name} started...")
-    
+    print(f"Driver {driver_name} started ...")
     udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    udp_socket.bind(("", cfg['UDP_PORT']))
-    print(f"Listening to Port {cfg['UDP_PORT']}...")
+    udp_socket.bind(("", cfg["UDP_PORT"]))
+    print(f"Server IP Address: {socket.gethostbyname(socket.gethostname())}")
+    print(f"Listening to Port {cfg['UDP_PORT']} on {udp_socket.getsockname()[0]} ...")
     lastLap = 0  # force a new lap after we pass start finish
-    sector3time = 0    
+    sector3TimeInS = 0
     # Receive Packages
     while True:
         udp_packet = udp_socket.recv(2048)
         packet = unpack_udp_packet(udp_packet)
 
-        packet_type = packet.header.packetId # get the packet type from the header
+        packet_type = packet.header.packetId  # get the packet type from the header
         position = packet.header.playerCarIndex # get the position of the driver in the list as the packet contains data of all driving cars
-        
+
         # LAP DATA
         if packet_type == 2:
             newLap = False
             car_laptime_data = packet.lapData[position]
-            if car_laptime_data.currentLapNum  > lastLap:
-               newLap = True
-               lastLap = car_laptime_data.currentLapNum
-               sector3time = car_laptime_data.lastLapTime - (car_laptime_data.sector1TimeInMS + car_laptime_data.sector1TimeInMS)
-               print(f"Starting Lap {lastLap}...")
-            elif  car_laptime_data.sector1TimeInMS == 0 and car_laptime_data.sector1TimeInMS == 0:
-                  sector3time = 0
-            metrics.write_lap_data_to_splunk(driver_name, car_laptime_data, sector3time) 
-         
+            if car_laptime_data.currentLapNum > lastLap:
+                newLap = True
+                lastLap = car_laptime_data.currentLapNum
+                sector3TimeInS = car_laptime_data.lastLapTime - (
+                    car_laptime_data.sector1TimeInMS + car_laptime_data.sector1TimeInMS
+                )
+                print(f"Starting Lap {lastLap}...")
+            elif (
+                car_laptime_data.sector1TimeInMS == 0
+                and car_laptime_data.sector1TimeInMS == 0
+            ):
+                sector3TimeInS = 0
+            metrics.write_lap_data_to_splunk(driver_name, car_laptime_data, sector3TimeInS)
 
         # TELEMETRY DATA
         if packet_type == 6:
--- telemetry/splunk.py
+++ telemetry/splunk.py
@@ -5,107 +5,123 @@ import yaml
 with open("config.yaml", "r") as ymlfile:
     cfg = yaml.safe_load(ymlfile)
 
-sfx = signalfx.SignalFx(api_endpoint='https://api.' + cfg['REALM'] + '.signalfx.com',
-        ingest_endpoint='https://ingest.' + cfg['REALM'] + '.signalfx.com',
-        stream_endpoint='https://stream.' + cfg['REALM'] + '.signalfx.com')
+sfx = signalfx.SignalFx(
+    api_endpoint="https://api." + cfg["REALM"] + ".signalfx.com",
+    ingest_endpoint="https://ingest." + cfg["REALM"] + ".signalfx.com",
+    stream_endpoint="https://stream." + cfg["REALM"] + ".signalfx.com",
+)
 
-ingest = sfx.ingest(cfg['ACCESS_TOKEN'])
+ingest = sfx.ingest(cfg["ACCESS_TOKEN"])
+
 
 def write_telemetry_data_to_splunk(driver_name, car_telemetry_data):
-    #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    print(car_telemetry_data.speed)
+    # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     ingest.send(
-        gauges=[{
-                'metric': 'f1_2021.speed',
-                'value': car_telemetry_data.speed,
-                'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.engineRPM',
-                'value': car_telemetry_data.engineRPM,
-                'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.gear',
-                'value': car_telemetry_data.gear,
-                'dimensions': {'driver': driver_name}
-            }
-            ,{
-                'metric': 'f1_2021.brake',
-                'value': car_telemetry_data.brake,
-                'dimensions': {'driver': driver_name}
-            }]
+        gauges=[
+            {
+                "metric": "f1_2021.speed",
+                "value": car_telemetry_data.speed,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.engineRPM",
+                "value": car_telemetry_data.engineRPM,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.gear",
+                "value": car_telemetry_data.gear,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.brake",
+                "value": car_telemetry_data.brake,
+                "dimensions": {"driver": driver_name},
+            },
+        ]
     )
 
     if car_telemetry_data.drs == 1:
         ingest.send_event(
-            event_type='drs_enabled',
-            category='USER_DEFINED',
-            dimensions={'driver': driver_name}
-    )
+            event_type="drs_enabled",
+            category="USER_DEFINED",
+            dimensions={"driver": driver_name},
+        )
 
-def write_lap_data_to_splunk(driver_name, car_laptime_data, sector3time):
-    #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+def write_lap_data_to_splunk(driver_name, car_laptime_data, sector3TimeInS):
+    # logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     ingest.send(
-        gauges=[{
-                'metric': 'f1_2021.currentLapNum',
-                'value': car_laptime_data.currentLapNum,
-                'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.lastLapTime',
-                'value': car_laptime_data.lastLapTime,
-                'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.currentLapTime',
-                'value': car_laptime_data.currentLapTime,
-                'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.bestLapTime',
-                'value': car_laptime_data.bestLapTime,
-                'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.bestLapNum',
-                'value': car_laptime_data.bestLapNum,
-                'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.sector1TimeInMS',
-                'value': car_laptime_data.sector1TimeInMS,
-                'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.sector2TimeInMS',
-                'value': car_laptime_data.sector2TimeInMS,
-                'dimensions': {'driver': driver_name}
+        gauges=[
+            {
+                "metric": "f1_2021.currentLapNum",
+                "value": car_laptime_data.currentLapNum,
+                "dimensions": {"driver": driver_name},
             },
-             {
-                 'metric': 'f1_2021.sector3TimeInMS',
-                 'value': sector3time,
-                  'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.bestOverallSector1TimeInMS',
-                'value': car_laptime_data.bestOverallSector2TimeInMS,
-                'dimensions': {'driver': driver_name}
-            }
-            ,{
-                'metric': 'f1_2021.bestOverallSector1LapNum',
-                'value': car_laptime_data.bestOverallSector2LapNum,
-                'dimensions': {'driver': driver_name}
+            {
+                "metric": "f1_2021.lastLapTime",
+                "value": car_laptime_data.lastLapTime,
+                "dimensions": {"driver": driver_name},
             },
-             {
-                'metric': 'f1_2021.bestOverallSector2TimeInMS',
-                'value': car_laptime_data.bestOverallSector2TimeInMS,
-                'dimensions': {'driver': driver_name}
-            }
-            ,{
-                'metric': 'f1_2021.bestOverallSector2LapNum',
-                'value': car_laptime_data.bestOverallSector2LapNum,
-                'dimensions': {'driver': driver_name}
-            },{
-                'metric': 'f1_2021.bestOverallSector3TimeInMS',
-                'value': car_laptime_data.bestOverallSector2TimeInMS,
-                'dimensions': {'driver': driver_name}
-            }
-            ,{
-                'metric': 'f1_2021.bestOverallSector3LapNum',
-                'value': car_laptime_data.bestOverallSector2LapNum,
-                'dimensions': {'driver': driver_name}
-            }
-            ]
+            {
+                "metric": "f1_2021.currentLapTime",
+                "value": car_laptime_data.currentLapTime,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.bestLapTime",
+                "value": car_laptime_data.bestLapTime,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.bestLapNum",
+                "value": car_laptime_data.bestLapNum,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.sector1TimeInMS",
+                "value": car_laptime_data.sector1TimeInMS,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.sector2TimeInMS",
+                "value": car_laptime_data.sector2TimeInMS,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.bestOverallSector1TimeInMS",
+                "value": car_laptime_data.bestOverallSector1TimeInMS,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.bestOverallSector1LapNum",
+                "value": car_laptime_data.bestOverallSector2LapNum,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.bestOverallSector2TimeInMS",
+                "value": car_laptime_data.bestOverallSector2TimeInMS,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.bestOverallSector2LapNum",
+                "value": car_laptime_data.bestOverallSector2LapNum,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.bestOverallSector3TimeInMS",
+                "value": car_laptime_data.bestOverallSector3TimeInMS,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.bestOverallSector3LapNum",
+                "value": car_laptime_data.bestOverallSector2LapNum,
+                "dimensions": {"driver": driver_name},
+            },
+            {
+                "metric": "f1_2021.sector3TimeInS",
+                "value": sector3TimeInS,
+                "dimensions": {"driver": driver_name},
+            },
+        ]
     )
